9edfcfe22cafefc0cb830fe50f337f3925c7d52c
Yashaswini2005 <yashaswinim.bsc23@rvu.edu.in>
2024-01-23 09:20:00 +0530

    committed 1/23/2024, 9:20:00 AM

---
+++ set01/problem04.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void input(int *n);
+void sum(int n, int *sum);
+void output(int n, int sum);
+int main(){
+    int n, add;
+    input(&n);
+    sum(n,&add);
+    output(n,add);
+    }
+    void input(int *n){
+        printf("enter the number:");
+        scanf("%d",n);
+    }
+    void sum(int n,int *sum){
+        *sum=0;
+        for(int i=0;i<n;i++){
+            *sum=*sum+i;
+        }
+    }
+    void output(int n, int sum){
+printf("the sum of natural n natural numbers are :%d",sum);
+    }
