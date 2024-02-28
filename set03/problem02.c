1c7191f01a205d398062685bb0145635b7cafaa7
Yashaswini2005 <yashaswinim.bsc23@rvu.edu.in>
2024-02-28 20:16:10 +0530

    committed 2/28/2024, 8:16:10 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -1,42 +1,38 @@
-#include <stdio.h>
-#include <math.h>
-
-// Function declarations
-void input(float *x1, float *y1, float *x2, float *y2);
-float find_distance(float x1, float y1, float x2, float y2);
-void output(float x1, float y1, float x2, float y2, float distance);
-
-int main() {
-    float x1, y1, x2, y2;
-    float distance;
-
-    // Input
-    input(&x1, &y1, &x2, &y2);
-
-    // Calculate distance
-    distance = find_distance(x1, y1, x2, y2);
-
-    // Output
-    output(x1, y1, x2, y2, distance);
-
+#include<stdio.h>
+#include<math.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3);
+void output(float x1, float y1, float x2, float y2, float x3, float y3, int result);
+int main(){
+    float x1,y1,x2,y2,x3,y3;
+    int result;
+    input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+    result=is_triangle(x1,y1,x2,y2,x3,y3);
+    output(x1,y1,x2,y2,x3,y3,result);
     return 0;
 }
-
-// Function to input coordinates
-void input(float *x1, float *y1, float *x2, float *y2) {
-    printf("Enter the coordinates of point 1 (x1 y1): ");
-    scanf("%f %f", x1, y1);
-
-    printf("Enter the coordinates of point 2 (x2 y2): ");
-    scanf("%f %f", x2, y2);
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
+    printf("Enter the coordinates of the first point (x1,y1)\n:");
+    scanf("%f,%f",x1,y1);
+    printf("Enter the coordinates of the Second  point (x2,y2)\n:");
+    scanf("%f,%f",x2,y2);
+    printf("Enter the coordinates of the Third point (x3,y3)\n:");
+    scanf("%f,%f",x3,y3);
 }
-
-// Function to calculate distance
-float find_distance(float x1, float y1, float x2, float y2) {
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3){
+    if((x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))!=0) {
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
+void output(float x1, float y1, float x2, float y2, float x3, float y3, int result){
+    if(result){
+        printf("the points (%f,%f),(%f,%f) and (%f,%f) Form a Triangle.:",x1,y1,x2,y2,x3,y3);
 
-// Function to output results
-void output(float x1, float y1, float x2, float y2, float distance) {
-    printf("Distance between (%.2f, %.2f) and (%.2f, %.2f) is %.2f\n", x1, y1, x2, y2, distance);
+    }
+    else{
+        printf("the points (%f,%f),(%f,%f) and (%f,%f)does not form a Triangle.:",x1,y1,x2,y2,x3,y3);
+    }
 }
