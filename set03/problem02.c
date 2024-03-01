a157f6253caa95046119be6f56ecb3f236fb7446
Yashaswini2005 <yashaswinim.bsc23@rvu.edu.in>
2024-03-01 14:00:21 +0530

    committed 3/1/2024, 2:00:21 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -1,38 +1,42 @@
 #include<stdio.h>
 #include<math.h>
+
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
 int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3);
 void output(float x1, float y1, float x2, float y2, float x3, float y3, int result);
+
 int main(){
-    float x1,y1,x2,y2,x3,y3;
+    float x1, y1, x2, y2, x3, y3;
     int result;
-    input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
-    result=is_triangle(x1,y1,x2,y2,x3,y3);
-    output(x1,y1,x2,y2,x3,y3,result);
+
+    input_triangle(&x1, &y1, &x2, &y2, &x3, &y3);
+    result = is_triangle(x1, y1, x2, y2, x3, y3);
+    output(x1, y1, x2, y2, x3, y3, result);
+
     return 0;
 }
+
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
-    printf("Enter the coordinates of the first point (x1,y1)\n:");
-    scanf("%f,%f",x1,y1);
-    printf("Enter the coordinates of the Second  point (x2,y2)\n:");
-    scanf("%f,%f",x2,y2);
-    printf("Enter the coordinates of the Third point (x3,y3)\n:");
-    scanf("%f,%f",x3,y3);
+    printf("Enter the coordinates of the first point (x1,y1):");
+    scanf("%f %f", x1, y1);
+    printf("Enter the coordinates of the Second  point (x2,y2):");
+    scanf("%f %f", x2, y2);
+    printf("Enter the coordinates of the Third point (x3,y3):");
+    scanf("%f %f", x3, y3);
 }
+
 int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3){
-    if((x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))!=0) {
+    if ((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) != 0) {
         return 1;
-    }
-    else{
+    } else {
         return 0;
     }
 }
+
 void output(float x1, float y1, float x2, float y2, float x3, float y3, int result){
     if(result){
-        printf("the points (%f,%f),(%f,%f) and (%f,%f) Form a Triangle.:",x1,y1,x2,y2,x3,y3);
-
-    }
-    else{
-        printf("the points (%f,%f),(%f,%f) and (%f,%f)does not form a Triangle.:",x1,y1,x2,y2,x3,y3);
+        printf("the points (%f,%f),(%f,%f) and (%f,%f) Form a Triangle.\n", x1, y1, x2, y2, x3, y3);
+    } else {
+        printf("the points (%f,%f),(%f,%f) and (%f,%f) do not form a Triangle.\n", x1, y1, x2, y2, x3, y3);
     }
 }
