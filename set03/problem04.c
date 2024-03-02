a45117162864b757a60546f629548b4512daad6f
Yashaswini2005 <yashaswinim.bsc23@rvu.edu.in>
2024-03-02 20:37:32 +0530

    committed 3/2/2024, 8:37:32 PM

---
+++ set03/problem04.c
@@ -0,0 +1,29 @@
+//fibonacci
+#include<stdio.h>
+int input();
+int find_fibo(int n);
+void output(int n, int fibo);
+int main(){
+    int n,fibo;
+    n=input();
+    fibo=find_fibo(n);
+    output(n,fibo);
+    return 0;
+}
+int input(){
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    return n;
+}
+int find_fibo(int n){
+    if(n<=1){
+        return n;
+    }
+    else{
+        return find_fibo(n-1)+find_fibo(n-2);
+    }
+}
+void output(int n,int fibo){
+    printf("The %dth number in the fibonacci sequence is %d:",n,fibo);
+}
